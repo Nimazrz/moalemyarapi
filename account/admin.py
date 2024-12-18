2b480b0bbe3f6b2fc2a69d111477283e7d99c334
Nimazrz <Nimaaa8413@gmail.com>
2024-12-19 03:27:33 +0330

    added account but buggy

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Question_designer
 
 # Register your models here.
+
+@admin.register(Question_designer)
+class Question_designerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'phone')
